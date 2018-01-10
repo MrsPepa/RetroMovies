dafe55e823d877735790644600ae8743c3ad777e
MrsPepa <jose.leon1996@gmail.com>
2018-01-10 09:30:13 -0300

    agregando logo y modificando icono de busqueda

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,29 @@
-$(function() { 
+$(function() {
 
-    $('a[href="#toggle-search"], .navbar-bootsnipp .bootsnipp-search .input-group-btn > .btn[type="reset"]').on('click', function(event) {
-		event.preventDefault();
-		$('.navbar-bootsnipp .bootsnipp-search .input-group > input').val('');
-		$('.navbar-bootsnipp .bootsnipp-search').toggleClass('open');
-		$('a[href="#toggle-search"]').closest('li').toggleClass('active');
+  $('a[href="#search"], .navbar-bootsnipp .bootsnipp-search .input-group-btn > .btn[type="reset"]').on('click', function(event) {
+  event.preventDefault();
+  $('.navbar-bootsnipp .bootsnipp-search .input-group > input').val('');
+  $('.navbar-bootsnipp .bootsnipp-search').toggleClass('open');
+  $('a[href="#toggle-search"]').closest('li').toggleClass('active');
 
-		if ($('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
-			/* I think .focus dosen't like css animations, set timeout to make sure input gets focus */
-			setTimeout(function() { 
-				$('.navbar-bootsnipp .bootsnipp-search .form-control').focus();
-			}, 100);
-		}			
-	});
+    if ($('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
+    /* I think .focus dosen't like css animations, set timeout to make sure input gets focus */
+      setTimeout(function() {
+        $('.navbar-bootsnipp .bootsnipp-search .form-control').focus();
+      }, 100);
+    }
+  });
 
-	$(document).on('keyup', function(event) {
-		if (event.which == 27 && $('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
-			$('a[href="#toggle-search"]').trigger('click');
-		}
-	});
-    
+  $(document).on('keyup', function(event) {
+    if (event.which == 27 && $('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
+      $('a[href="#toggle-search"]').trigger('click');
+    }
+  });
 });
-
+/*funcion para abrir buscador*/
+$('.animate').click(function(){
+  
+});
 /*aparece login*/
  $('#loginmainnvb').click(function(){
   $('.contlog').append(`<section id="lab_video_slider" class="login-form-link">
